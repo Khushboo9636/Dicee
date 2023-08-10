9d94f9033fafa5b2507c97c5bd6f2dd00dda5ef0
Khushboo9636 <Sinhakhushboo31@gmail.com>
2023-08-10 17:41:45 +0530

    disc

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Khushboo on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
